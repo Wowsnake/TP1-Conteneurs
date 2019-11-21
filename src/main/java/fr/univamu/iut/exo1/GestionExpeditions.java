b010f4a077ff4020766fb7be7db352109d06787c
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-11-21 15:21:30 +0100

    EXO 1

--- src/main/java/fr/univamu/iut/exo1/GestionExpeditions.java
+++ src/main/java/fr/univamu/iut/exo1/GestionExpeditions.java
@@ -2,6 +2,9 @@ package fr.univamu.iut.exo1;
 
 public class GestionExpeditions {
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+        Lecteur info = new Lecteur();
+        info.charger("Conteneurs.ser");
+        //throw new RuntimeException("Not yet implemented !");
+
     }
 }
--- src/main/java/fr/univamu/iut/exo1/Lecteur.java
+++ src/main/java/fr/univamu/iut/exo1/Lecteur.java
@@ -1,12 +1,33 @@
+
 package fr.univamu.iut.exo1;
 
 import fr.univamu.iut.Conteneur;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+
 
 public class Lecteur {
     public static Collection<Conteneur> charger(String fichier) {
-        // À compléter
+        try {
+            FileInputStream fis = new FileInputStream(fichier);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Conteneur obj=(Conteneur)ois.readObject();
+            ArrayList<String> infocont = new ArrayList<String>();
+            infocont.add(obj.toString());
+
+
+            for (int i = 0; i < infocont.size(); i++) {
+                System.out.println(infocont.get(i));
+            }
+            fis.close();
+            ois.close();
+        } catch ( Exception exception ) {
+            System.err.println( "Cannot read objects from the stream" );
+            exception.printStackTrace();
+        }
         return null;
     }
 }
